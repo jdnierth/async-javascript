3118089b82e9bcb1dacb7b4f50272e14d6fd8a05
jdmeinhold <jdmeinhold@web.de>
2024-04-20 18:35:18 +0200

    added pipe exercise

---
+++ src/pipes/README.md
@@ -0,0 +1,6 @@
+# Prime numbers
+
+1. Create an Observable consisting of the following prime numbers `2, 3, 5, 7, 11, 13, 17, 19, 23`
+2. Filter out all even numbers
+3. Take the first 5 numbers
+4. Add all numbers up and print the result on the console
---
+++ src/pipes/exercisePipe.ts
@@ -0,0 +1,16 @@
+import { from } from "rxjs";
+import { filter, reduce, take, tap } from "rxjs/operators";
+
+const primes = from([2, 3, 5, 7, 11, 13, 17, 19, 23]);
+
+primes
+  .pipe(
+    filter((prime) => prime % 2 !== 0),
+    take(5),
+    tap((res) => console.log("Picked: ", res)),
+    reduce((sum, acc) => sum + acc, 0)
+  )
+  .subscribe({
+    next: (result) => console.log("Result: ", result),
+    error: (error) => console.log("Error: ", error),
+  });
